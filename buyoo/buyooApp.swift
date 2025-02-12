b9e2d3d4bd95e9dbeafc128141a2280dbc20d995
afzal_ahmed <ahmed.iosdev101@gmail.com>
2025-02-12 20:55:47 +0500

    Initial Commit

---
+++ buyoo/buyooApp.swift
@@ -0,0 +1,17 @@
+//
+//  buyooApp.swift
+//  buyoo
+//
+//  Created by Apple on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct buyooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
